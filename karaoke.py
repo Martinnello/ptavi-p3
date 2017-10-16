ca5f4a2cbf373c6e559140d7904314f74cc6dc99
Martinnello <32375133+Martinnello@users.noreply.github.com>
2017-10-16 22:16:50 +0200

    Final karaoke
    ok

--- karaoke.py
+++ karaoke.py
@@ -4,15 +4,11 @@
 import smallsmilhandler
 import sys
 import json
-<<<<<<< HEAD
 import urllib.request
-=======
->>>>>>> e37c1fbb099b2e25c0ee28c38c67176c9f2cfcc8
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-<<<<<<< HEAD
 class KaraokeLocal:                         # TAKE SMIL FILE & READ IT
 
     def __init__(self):
@@ -76,44 +72,3 @@ if __name__ == "__main__":
     Smil.do_local()
     Smil.to_json(sys.argv[1], "local.json")
     Smil.__str__()
-=======
-if __name__ == "__main__":
-
-    try:
-        File = sys.argv[1]
-        Fich = open(File)
-        Fich.close()
-    except FileNotFoundError:
-        sys.exit("	 File not found!")
-    except IndexError:
-        sys.exit("    Usage:   python3 karaoke.py file.smil")
-
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSmilHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(File))
-
-    Lista = cHandler.get_tags()
-    Descripcion = ""
-
-    for name in Lista:
-
-        Elemento = name[0]
-        Atributos = name[1]
-        Descripcion += Elemento + "\t"
-
-        for Atributo in Atributos:
-            Valor = Atributos[Atributo]
-
-            if Valor != "":
-                Descripcion += Atributo + '=' + Valor + "\t"
-
-        Descripcion += "\n"
-
-    print(Descripcion)
-
-    FichJson = File.replace(".smil", ".json")
-
-    with open(FichJson, "w") as jsonfile:
-        json.dump(Lista, jsonfile, indent=3)
->>>>>>> e37c1fbb099b2e25c0ee28c38c67176c9f2cfcc8
